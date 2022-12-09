eea70d96b51fff52892d10eadc98d945d9824421
pountzas <nikos@pountzas.gr>
2022-12-09 19:11:13 +0200

    add noLogo function for table

---
+++ components/NoLogo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Vendor from '../public/assets/svg/icons/vendor'
+
+function NoLogo() {
+  return (
+    <div className='w-6 h-6 rounded-full p-[5px] bg-gray-200 flex items-center justify-center'>
+      <Vendor />
+    </div>
+  )
+}
+
+export default NoLogo
